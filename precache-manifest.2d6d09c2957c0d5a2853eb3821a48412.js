d354e1b310d0d79703d21386c5b0b7358fa1faf8
Larry <pascu.laurentiu17@gmail.com>
2019-11-28 15:48:19 +0200

    Updates

---
+++ precache-manifest.2d6d09c2957c0d5a2853eb3821a48412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2223e8ce1f3d4653194689fc20bc7070",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "ba128def5a1f2b5c118e",
+    "url": "/todoapp/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e1a5ebcdc5c024b07498",
+    "url": "/todoapp/static/js/2.ad3b01b9.chunk.js"
+  },
+  {
+    "revision": "ba128def5a1f2b5c118e",
+    "url": "/todoapp/static/js/main.74da5978.chunk.js"
+  },
+  {
+    "revision": "d09d12b45e68eae5bbf3",
+    "url": "/todoapp/static/js/runtime-main.5e17cae6.js"
+  }
+]);
